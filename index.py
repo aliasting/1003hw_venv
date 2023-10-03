492680120ea1f41b993436f292c26b8218f54d2d
aliasting <alias0305@gmail.com>
2023-10-03 17:38:32 +0800

    4454

---
+++ index.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.jinja.html")
+
+@app.route("/hello_world")
+def hello_world():
+    return "<h1>Hello! World.</h1>"
+
